f7011f85b5c1d47ca56a2a39dbc1cc381c413035
unknown <huynhchitrung020503@gmail.com>
2023-11-05 14:35:02 +0700

    Setup enviroment

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
+        HELLO WORLD
+      </div>
+    </>
+  );
+}
+
+export default App;
